5faec71b3609b2906b2ba491eaa0ea1a9c2e5723
Hubert <hubhcsi@gmail.com>
2023-01-23 20:50:05 -0500

    add files

---
+++ nuxt-app/nuxt.config.ts
@@ -0,0 +1,30 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/i18n',
+  ],
+  i18n: {
+    strategy: 'prefix',
+    baseUrl: '/2023/',
+    defaultLocale: 'en',
+    locales: [
+      {
+        code: 'en',
+      },
+      {
+        code: 'fr',
+      }, 
+    ],
+    vueI18n: {
+      legacy: false,
+      messages: {
+        en: {
+          welcome: 'Welcome'
+        },
+        fr: {
+          welcome: 'Bienvenue'
+        }
+      }
+    }
+  }
+})
---
+++ nuxt-app/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <a class="menuitem lang" v-for="l in availableLocales()" :key="l.code" :href="switchLocalePath(l.code)">
+      {{ l.code.toUpperCase() }}
+    </a>
+  </div>
+</template>
+
+<script setup>
+
+const { locale, locales } = useI18n()
+
+const switchLocalePath = useSwitchLocalePath()
+
+const availableLocales = () => {
+  return (locales.value).filter(i => i.code !== locale.value)
+}
+
+</script>
